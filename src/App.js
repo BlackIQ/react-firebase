89a8876ebecbfde4c9bf4d4faf20bcd46c930235
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-27 01:51:47 +0430

    Stuff

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Login from "./pages/login";
+import Navbar from "./components/navbar";
+import Register from "./pages/register";
+import Reset from "./pages/reset";
 
 function App() {
-  return (
-      <React.Fragment>
-        <p>Firebase</p>
-      </React.Fragment>
-  );
+    return (
+        <Router>
+            <Navbar />
+            <div className='container py-4'>
+                <Switch>
+                    <Route exact path='/'>Index</Route>
+                    <Route exact path='/login'><Login /></Route>
+                    <Route exact path='/register'><Register /></Route>
+                    <Route exact path='/reset'><Reset /></Route>
+                    <Route exact path='/home'>Home</Route>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
