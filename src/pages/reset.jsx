31804565a46ca7cf72ae72cce18517a7bf17627a
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-27 02:59:17 +0430

    Added reset backend

--- src/pages/reset.jsx
+++ src/pages/reset.jsx
@@ -1,6 +1,23 @@
-import {Link} from "react-router-dom";
+import {Link, useHistory} from "react-router-dom";
+import {useEffect, useState} from "react";
+
+import {auth, sendPasswordReset} from "../firebase/firebase";
+import {useAuthState} from "react-firebase-hooks/auth";
+
 
 const Reset = () => {
+
+    const [email, setEmail] = useState('');
+
+    const [user, loading, error] = useAuthState(auth);
+
+    const history = useHistory();
+
+    useEffect(() => {
+        if (loading) return;
+        if (user) history.push('/home');
+    }, [user, loading])
+
     return (
         <div>
             <h2>Recover your account</h2>
@@ -10,12 +27,12 @@ const Reset = () => {
                     <div className='m-1'>
                         <h4 className='text-info'>Reset your password</h4>
                         <p>Just enter the email and we will handle it.</p>
-                        <form>
+                        <div>
                             <label className='form-label' htmlFor='email'>Email</label>
-                            <input placeholder='Email' className='form-control' type='email' id='email'/>
+                            <input placeholder='Email' className='form-control' type='email' id='email' onChange={e => setEmail(e.target.value)}/>
                             <br />
-                            <button className='btn btn-info w-100'>Send reset password</button>
-                        </form>
+                            <button onClick={() => sendPasswordReset(email)} className='btn btn-info w-100'>Send reset password</button>
+                        </div>
                         <br />
                         <p>If you remembered password, <Link to='/login' className='text-info'>login now</Link></p>
                     </div>
