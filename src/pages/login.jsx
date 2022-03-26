b04f3953cad939e5a1a7ab3c9cdd39029d89b2da
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-27 02:58:48 +0430

    Added login backend

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,7 +1,24 @@
 import {FaGoogle} from "react-icons/fa";
-import {Link} from "react-router-dom";
+import {Link, useHistory} from "react-router-dom";
+import {useState, useEffect} from "react";
+
+import {auth, loginWithEmailPassword, signInWithGoogle} from "../firebase/firebase";
+import {useAuthState} from "react-firebase-hooks/auth";
 
 const Login = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [user, loading, error] = useAuthState(auth);
+
+    const history = useHistory();
+
+    useEffect(() => {
+        if (loading) return;
+        if (user) history.push('/home');
+    }, [user, loading])
+
     return (
         <div>
             <h2>Login to you account</h2>
@@ -11,15 +28,15 @@ const Login = () => {
                     <div className='m-1'>
                         <h4 className='text-info'>Authenticate with Email</h4>
                         <p>Authenticate with your Email and password</p>
-                        <form>
+                        <div>
                             <label className='form-label' htmlFor='email'>Email</label>
-                            <input placeholder='Email' className='form-control' type='email' id='email'/>
+                            <input placeholder='Email' className='form-control' type='email' id='email' onChange={e => setEmail(e.target.value)}/>
                             <br />
                             <label className='form-label' htmlFor='password'>Password</label>
-                            <input placeholder='Password' className='form-control' type='password' id='password'/>
+                            <input placeholder='Password' className='form-control' type='password' id='password' onChange={e => setPassword(e.target.value)}/>
                             <br />
-                            <button className='btn btn-info w-100'>Login</button>
-                        </form>
+                            <button onClick={() => loginWithEmailPassword(email, password)} className='btn btn-info w-100'>Login</button>
+                        </div>
                         <br />
                         <p>If you do not have an account, <Link to='/register' className='text-info'>create now</Link></p>
                         <p>Forgot your password, <Link to='/reset' className='text-info'>reset it now</Link></p>
@@ -29,7 +46,7 @@ const Login = () => {
                     <div className='m-1'>
                         <h4 className='text-danger'>Or login with Google</h4>
                         <p>You can use your Google account to login too.</p>
-                        <button className='btn btn-danger w-100'><FaGoogle className='float-start' /><span className='float-end'>Sign in with Google</span></button>
+                        <button onClick={() => signInWithGoogle()} className='btn btn-danger w-100'><FaGoogle className='float-start' /><span className='float-end'>Sign in with Google</span></button>
                     </div>
                 </div>
             </div>
