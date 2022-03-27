ca3c79bc2ccbcd159e3ac8dc124d3fa9a3630814
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-27 10:39:41 +0430

    Remove fetch name

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,38 +1,23 @@
-import {useState, useEffect} from "react";
+import {useEffect} from "react";
 
+import {auth, logout} from "../firebase/firebase";
 import {useAuthState} from "react-firebase-hooks/auth";
 
-import {auth, db, logout} from "../firebase/firebase";
-import { query, collection, getDocs, where } from "firebase/firestore";
 import {useHistory} from "react-router-dom";
 
 const Home = () => {
 
     const [user, loading, error] = useAuthState(auth);
-    const [name, setName] = useState("");
     const history = useHistory();
-
-    const fetchUserName = async () => {
-        try {
-            const q = query(collection(db, "/users"), where("uid", "==", user?.uid));
-            const doc = await getDocs(q);
-            const data = doc.docs[0].data();
-            setName(data.name);
-        } catch (error) {
-            console.error(error);
-            alert(error.message);
-        }
-    };
 
     useEffect(() => {
         if (loading) return;
-        if (!user) return history.push('/login')
-        fetchUserName();
-    }, [user, loading]);
+        if (!user) history.push('/login');
+    }, [user, loading])
 
     return (
         <div>
-            <h1>Welcome <b>{name}</b> <button onClick={logout} className='btn btn-danger float-end'>Logout</button></h1>
+            <h1>Welcome <b>{user.displayName}</b> <button onClick={() => logout()} className='btn btn-danger float-end'>Logout</button></h1>
             <br />
         </div>
     );
