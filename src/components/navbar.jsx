71864839171f1b0ecff180cfeaf84f46ba234f16
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-27 10:58:04 +0430

    Move login and logout in navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,24 @@
-import {Link} from "react-router-dom";
+import {Link, useHistory} from "react-router-dom";
 import {FaFire, FaReact} from "react-icons/fa";
+import {useAuthState} from "react-firebase-hooks/auth";
+import {useEffect, useState} from "react";
+import {auth, logout} from "../firebase/firebase";
 
 const Navbar = () => {
+
+    const [navItem, setNavItem] = useState('');
+
+    const [user, loading, error] = useAuthState(auth);
+
+    useEffect(() => {
+        if (loading) return;
+        if (!user) {
+            setNavItem(<Link className='text-primary' to='/login'>Login</Link>);
+        } else {
+            setNavItem(<span className='pointer' onClick={() => logout()}>Logout</span>);
+        }
+    }, [user, loading])
+
     return (
         <nav className='navbar navbar-light bg-light'>
             <div className='container'>
@@ -10,6 +27,7 @@ const Navbar = () => {
                     &nbsp;+&nbsp;
                     <span className='text-info'><FaReact />&nbsp;ReactJs</span>
                 </Link>
+                {navItem}
             </div>
         </nav>
     );
